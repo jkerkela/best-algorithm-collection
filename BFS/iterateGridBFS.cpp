1ed0d0163e90fc277b7ed4e624c6f6836d72b555
Joni Kerkelä <jkerkela@users.noreply.github.com>
2020-04-10 17:46:42 +0300

    Update and rename iterategridBFS.cpp to iterateGridBFS.cpp

--- BFS/iterateGridBFS.cpp
+++ BFS/iterateGridBFS.cpp
@@ -10,13 +10,14 @@ public:
     int stepsNeededForSpreadingRotOverGrid(vector<vector<int>>& grid) {
         int gridWidth = grid[0].size();
         int gridHeight = grid.size();
-        int currentSteps = -1;
+        int currentSteps = 0;
         int currentFreshItemCount = 0;
         queue<pair<int,int>> rottenItemLocations;
-        addCurrentRotItemsToQueue(grid, rottenItemLocations, currentFreshItemCount, gridWidth, gridHeight);
+        initializeRotAndFreshItemsData(grid, rottenItemLocations, currentFreshItemCount, gridWidth, gridHeight);
         pair<int,int> currentRotItemLocation = rottenItemLocations.front();
         while(!rottenItemLocations.empty()) {
             int levelSize = rottenItemLocations.size();
+            bool rottenItemsThisLevel = false;
             for(int i = 0; i < levelSize; i++) {
                 pair<int,int> currentRotItemLocation = rottenItemLocations.front();
                 rottenItemLocations.pop();
@@ -29,22 +30,19 @@ public:
                         grid[adjacentYCoord][adjacentXCoord] = 2;
                         currentFreshItemCount--;
                         rottenItemLocations.push({adjacentYCoord, adjacentXCoord});
+                        rottenItemsThisLevel = true;
                     } 
                 }
             }
-            currentSteps++;
+            if(rottenItemsThisLevel) {currentSteps++;}
         }
-        
         if(currentFreshItemCount > 0) {
             return -1;
-        }
-        if (currentSteps == -1) {
-            return 0;
         }
         return currentSteps;
     }
     
-    void addCurrentRotItemsToQueue(vector<vector<int>>& grid, queue<pair<int,int>>& rottenItemLocations, int& currentFreshItemCount, int& gridWidth, int& gridHeight) {
+    void initializeRotAndFreshItemsData(vector<vector<int>>& grid, queue<pair<int,int>>& rottenItemLocations, int& currentFreshItemCount, int& gridWidth, int& gridHeight) {
         for(int  i = 0; i < gridHeight; i++) {
             for(int j = 0; j < gridWidth; j++) {
                 if(grid[i][j] == 2) {
