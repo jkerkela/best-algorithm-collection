5efc07acd6b31a7d8f6f1c48349fa1dcf7750175
Joni Kerkelä <jkerkela@users.noreply.github.com>
2020-05-09 21:59:35 +0300

    Create inorderTraverseTree.cpp

---
+++ BST/inorderTraverseTree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        traverseInorderRecursively(root, result);
+        return result;
+    }
+    
+    void traverseInorderRecursively(TreeNode* node, vector<int>& result) {
+        if(node == NULL) return;
+        traverseInorderRecursively(node->left, result);
+        result.push_back(node->val);
+        traverseInorderRecursively(node->right, result);
+    }
+};
